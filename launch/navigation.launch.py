7d186a31d99ac261b4e34ff8cdcc5b65caacfbb4
MatthewZid <m_zidianakis@hotmail.com>
2023-04-03 23:58:21 +0300

    added slam and nav2 launch files

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import os
 import launch
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction, RegisterEventHandler
+from launch.event_handlers import OnProcessStart, OnProcessExit
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
@@ -13,10 +14,24 @@ def generate_launch_description():
     declared_argument = DeclareLaunchArgument(name='use_sim_time', default_value='True',
                                             description='Flag to enable use_sim_time')
     
-    kart_sim_launch = IncludeLaunchDescription(
+    slam_argument = DeclareLaunchArgument(name='slam_params_file', default_value=PathJoinSubstitution([FindPackageShare('kart_navigation'), 'config/slam.yaml']),
+                                            description='Slam yaml config file')
+    
+    nav_argument = DeclareLaunchArgument(name='nav_params_file', default_value=PathJoinSubstitution([FindPackageShare('kart_navigation'), 'config/nav2_params.yaml']),
+                                            description='Nav2 yaml config file')
+
+    slam_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution([FindPackageShare('kart_simulation'), 'launch/kart_kartland_simulation.launch.py'])
-        )
+            PathJoinSubstitution([FindPackageShare('slam_toolbox'), 'launch/online_async_launch.py'])
+        ),
+        launch_arguments={'slam_params_file': LaunchConfiguration('slam_params_file')}.items()
+    )
+
+    nav_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([FindPackageShare('nav2_bringup'), 'launch/navigation_launch.py'])
+        ),
+        launch_arguments={'params_file': LaunchConfiguration('nav_params_file'), 'use_sim_time': LaunchConfiguration('use_sim_time')}.items()
     )
     
     robot_localization_node = Node(
@@ -29,7 +44,10 @@ def generate_launch_description():
 
     return launch.LaunchDescription([
         declared_argument,
-        kart_sim_launch,
-        robot_localization_node
+        slam_argument,
+        nav_argument,
+        robot_localization_node,
+        slam_launch,
+        nav_launch
     ])
 
